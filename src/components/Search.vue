f1a4bcab2aeb8e94e43f915a830062a374cd28d1
faselts <faselts12857600@gmail.com>
2022-04-27 00:17:36 +0800

    search

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,106 +1,64 @@
 <template>
-  <div class="formAbsolute">
-    <div class="form">
-      
-        <input type="text" placeholder="input" autocomplete="off" required />
-  
-     
-
- 
-
-      <label for="name" class="label-name">
-        <span class="content-name">Search</span>
-      </label>
-    </div>
+  <div class="pagination">
+    <select  id="select" v-model="input.page" v-on:change="GoTo"></select>
+    <input type="checkbox" name="sex" v-model="input.sex" v-on:change="GoTo">
+    <label for="sex">Gender</label>
   </div>
 </template>
 
 <script>
-import TimelineLite from "gsap";
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+Vue.use(VueAxios, axios);
 
 export default {
-  name: "Search",
+  name: "Pagination",
+  data(){
+    return{
+      input:{
+        page:null,
+        sex:null,
+        name:null
+      }
+    }
+  },
   mounted() {
-    this.fullpage();
+    var count;
+    Vue.axios.get("http://140.123.174.200/api/Lawyer").then((resp) => {
+      this.count = resp.data.pageCount;
+      count = resp.data.pageCount;
+
+      const container = document.getElementById("select");
+
+      for (var i = 1; i <= count - 0; i++) {
+        var options = document.createElement("option");
+        options.value = i;
+        options.innerHTML = i;
+        container.appendChild(options);
+      }
+    });
   },
-  methods: {
-    fullpage() {
-      TimelineLite.fromTo(
-        ".formAbsolute",
-        1.4,
-        { y: "-10vh", opacity: 0 },
-        { y: "0%", opacity: 1, ease: TimelineLite.easeInOut },
-        "+=5"
-      );
+  methods:{
+     GoTo(){
+      this.$emit('changeurll',{page:this.input.page,sex:this.input.sex})
     },
-  },
+    
+  }
 };
 </script>
 
 <style scoped>
-.formAbsolute {
-  border-radius: 2rem;
-  display: flex;
-  z-index: 0;
-  position: absolute;
-  width: 45%;
-  top: 20vh;
-  left: 28%;
-  height: 60px;
-  background-color: white;
-  justify-content: center;
-  align-items: center;
-  min-width: 100px;
+.pagination{
+  width: 100vw;
+  height: 100vh;
 }
-.form {
-  width: 90%;
-  position: relative;
-  height: 50px;
-  border-radius: 2rem;
-  left: 0px;
-  top: 0;
+button {
+  width: 100px;
+  height: 100px;
 }
-.form input {
-  width: 100%;
-  height: 100%;
-  color: #595f6e;
-  padding-top: 20px;
-  border: none;
-  outline: none;
-  border-radius: 2rem;
-}
-.form label {
-  position: absolute;
-  pointer-events: none;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  border-radius: 2rem;
-  border-bottom: 2px solid #595f6e;
-}
-.content-name {
-  color: #595f6e;
-  position: absolute;
-  bottom: 0.65rem;
-  left: 0px;
-  transition: all 0.3s ease;
-}
-.form input:focus + .label-name .content-name,
-.form input:valid + .label-name .content-name {
-  transform: translateY(-130%);
-  font-size: 14px;
-  color: #5fa8d3;
-}
-.form input:focus + .searchicon,
-.form input:valid + .searchicon {
-  border: 10px solid #5fa8d3;
-}
-.searchicon {
-  position: absolute;
-  width: 1.5rem;
-  height: 1.5rem;
-  top: 30%;
-  right: 0%;
-  opacity: 0.5;
+select {
+  width: 50px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,11 +5,15 @@ import App from './App.vue'
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 import VueRouter from'vue-router'
-import PageNotFound from './pages/PageNotFound.vue'
+// import PageNotFound from './pages/PageNotFound.vue'
 import Home from './pages/Home.vue'
 import About from './pages/About.vue'
 import Works from './pages/Works.vue'
 import Contactus from './pages/Contactus.vue'
+import Vuex from 'vuex'
+
+
+Vue.use(Vuex)
 
 Vue.use(VueRouter)
 
@@ -18,7 +22,7 @@ const routes=[
   {path:'/about',component:About},
   {path:'/works',component:Works},
   {path:'/contactus',component:Contactus},
-  {path:'*', component:PageNotFound}
+  // {path:'*', component:PageNotFound}
 ]
 const router = new VueRouter({
   routes
