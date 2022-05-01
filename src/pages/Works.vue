6e4c8b30af85767f11f132fb9af1ac2c9f896a6d
faselts <faselts12857600@gmail.com>
2022-05-01 12:12:37 +0800

    search

--- src/pages/Works.vue
+++ src/pages/Works.vue
@@ -1,6 +1,20 @@
 <template>
-    <div>
-        <h1>hello</h1>
+    <div class="result">
+      <div class="first">
+        <button @click="turn(resultp.first)">
+         {{this.$store.state.resultp.first}}
+        </button>
+      </div>
+      <div class="second">
+        <button @click="turn(resultp.second)">
+          {{ this.$store.state.resultp.second }}
+        </button>
+      </div>
+      <div class="third">
+        <button @click="turn(resultp.third)">
+          {{ this.$store.state.resultp.third }}
+        </button>
+      </div>
     </div>
 </template>
 
@@ -9,13 +23,13 @@
 
 export default {
     name: "Works",
-    mounted(){
-        console.log(this.result)
-        console.log("oops")
+    data(){
+        
     },
-    props:{
-        result:undefined
-    }
+    mounted(){
+        
+    },
+    
 }
 </script>
 
@@ -24,5 +38,26 @@ div{
     height: 30vh;
     width: 100vh;
     background-color: #fff;
+}
+
+.result {
+  height: 70vh;
+  width: 80vw;
+  background-color: rgb(153, 90, 90);
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  position: absolute;
+  top: 20vh;
+  z-index: 2;
+  left: 10vw;
+
+  color: #000;
+  
+}
+button {
+  width: 100px;
+  height: 100px;
+  color: #000;
 }
 </style>
